babbe91729a9545a8d75aa865158caaf4132b04d
David Cooper <dmcooper2412@gmail.com>
2026-03-23 21:27:38 -0700

    fleshed out level 4, added spike png

--- path_follow_2d.gd
+++ path_follow_2d.gd
@@ -5,3 +5,5 @@ extends PathFollow2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	self.progress += speed * delta
+	# so i don't keep a high number in memory if they stay on the screen
+	self.rotation = int(self.rotation + 30) % 450
